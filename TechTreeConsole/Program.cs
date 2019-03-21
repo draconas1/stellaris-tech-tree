f7333f67f72e867cfdb603fdbe21000a3f7e90a8
Draconas <draconas@gmail.com>
2019-03-21 23:33:52 +0000

    Minor Qol improvements

--- TechTreeConsole/Program.cs
+++ TechTreeConsole/Program.cs
@@ -13,20 +13,24 @@ using TechTreeCreator.Logger;
 namespace TechTreeConsole {
     class Program {
         private const string STELLARIS_ROOT_WINDOWS = "C:/Games/SteamLibrary/steamapps/common/Stellaris";
-        private const string STELLARIS_ROOT_MAC = "/Users/christian/Library/Application Support/Steam/steamapps/common/Stellaris";
-        
+        private const string STELLARIS_ROOT_MAC = "/Users/christian/Library/Application Support/Steam/steamapps/common/Stellaris";       
+
         private const string OUTPUT_WINDOWS = "C:/Users/Draconas/source/repos/stellaris-tech-tree/www";
         private const string OUTPUT_MAC = "/Users/christian/dev/graph/stellaris-tech-tree/www";
-        
-        
+
+        private const string STELLARIS_USER_WINDOWS = @"D:\Users\Draconas\Documents\Paradox Interactive\Stellaris";
+        private const string STELLARIS_USER_MAC = "/Users/christian/Documents/Paradox Interactive/Stellaris";
+
         static void Main(string[] args) {
             try {
                 string rootDir = STELLARIS_ROOT_WINDOWS;
                 string outputDir = OUTPUT_WINDOWS;
                 string modFileSuffix = "-windows";
+                string stellarisUserDir = STELLARIS_USER_WINDOWS;
                 if (args.Length > 0 && args[0].Equals("mac", StringComparison.InvariantCultureIgnoreCase)) {
                     rootDir = STELLARIS_ROOT_MAC;
                     outputDir = OUTPUT_MAC;
+                    stellarisUserDir = STELLARIS_USER_MAC;
                     modFileSuffix = "-mac";
                 }
 
@@ -41,7 +45,7 @@ namespace TechTreeConsole {
                     .WriteTo.Console(LogEventLevel.Debug, outputTemplate, theme: AnsiConsoleTheme.Literate)
                     .CreateLogger();
 
-                // IList<ModDirectoryHelper.ModFile> modFiles = ModDirectoryHelper.GetMods(@"/Users/christian/Documents/Paradox Interactive/Stellaris/");
+                //IList<ModDirectoryHelper.ModFile> modFiles = ModDirectoryHelper.GetMods(stellarisUserDir);
 
                 //ModDirectoryHelper.WriteModListFile(Path.Combine(outputDir, "mods.json"), modFiles);
 
